52ca9342222cff5f78fb61fb238c6c433c55848c
Khang Nguyen <nguyenkhang10a@gmail.com>
2021-10-16 04:36:49 +0930

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import * as io from "socket.io-client";
+import { useState } from 'react';
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
+
+  // create condition which not allow users to chat without a user name
+
+  const joinRoom = () => {
+    if (username !== '' && room !== '') {
+      socket.emit('join_room', room);
+    } 
+  };
+  
+
+  return (
+    <div className='App'>
+      <div className='chatheader'>
+        <h1> Welcome To Chat App</h1>
+      </div>
+
+      <div className='logIn'>
+        <div className='inputs'>
+          <input type='text' placeholder='Enter User Name' onChange = {(event) => {setUsername(event.target.value)}}/>
+          <input type='text' placeholder='Room ID' onChange = {(e) => {setRoom(e.target.value)}}/>
+        </div>
+        <button onClick = {joinRoom}>Enter Chat</button>
+
+      </div>
+        
+    </div>
+  );
+
+}
+
+
+export default App;
