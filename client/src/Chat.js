288998ffe25802f2fc05985fd9f4cd5181fbb9db
Khang Nguyen <nguyenkhang10a@gmail.com>
2021-11-04 00:38:15 +0930

    Create the chat function
    Create the chat function, allow user to on click the button and pass the information regarding the name, room id, time line, etc and emit to the back end

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useState } from 'react';
 
 function Chat({socket, username, room})  {
+    const [currentMessage, setCurrentMessage] = useState("");
+    const sendMessage = async () => {
+        if (currentMessage !== "") {
+            const messageData = {
+                room: room,
+                author: username,
+                message: currentMessage,
+                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+            };
+
+            await socket.emit('send_message', messageData);
+        }
+    };
     return (
         <div>
             <div className="chat-header">
@@ -8,8 +22,8 @@ function Chat({socket, username, room})  {
             </div>
             <div className="chat-body"></div>
             <div className="chat-footer">
-                <input type="text" placeholder="I am..."/>
-                <button> &#9658; </button>
+                <input type="text" placeholder="I am..." onChange = {(e) => {setCurrentMessage(e.target.value)}}/>
+                <button onClick = {sendMessage}> &#9658; </button>
             </div>
 
         </div>
