50bf2f04eb83646bb14adb0e5266413e2f7c1c31
Khang Nguyen <nguyenkhang10a@gmail.com>
2021-11-04 13:36:00 +0930

    App.js updated new function
    Allow users to shift to the chat page when ever enter sufficient info and click enter chat button

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import * as io from "socket.io-client";
 import { useState } from 'react';
 import Chat from './Chat.js';
 
+
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
+  const [showChat, setShowChat] = useState(false);
 
 
   // create condition which not allow users to chat without a user name
@@ -15,29 +17,37 @@ function App() {
   const joinRoom = () => {
     if (username !== '' && room !== '') {
       socket.emit('join_room', room);
+      setShowChat(true);
     }
   };
 
 
   return (
-    <div className='App'>
-      <div className='chatheader'>
-        <h1> Welcome To Chat App</h1>
+    <div className="App">
+    {!showChat ? (
+      <div className="joinChatContainer">
+        <h3>Join A Chat</h3>
+        <input
+          type="text"
+          placeholder="John..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Room ID..."
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <button onClick={joinRoom}>Join A Room</button>
       </div>
-
-      <div className='logIn'>
-        <div className='inputs'>
-          <input type='text' placeholder='Enter User Name' onChange = {(event) => {setUsername(event.target.value)}}/>
-          <input type='text' placeholder='Room ID' onChange = {(e) => {setRoom(e.target.value)}}/>
-        </div>
-        <button onClick = {joinRoom}>Enter Chat</button>
-        <Chat socket={socket} username={username} room={room} />
-
-      </div>
-
-    </div>
-  );
-
+    ) : (
+      <Chat socket={socket} username={username} room={room} />
+    )}
+  </div>
+);
 }
 
 
